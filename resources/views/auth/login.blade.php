1d11317c464d898074887a51779a5a25794f2c98
Asela054 <asela.indrajith@gmail.com>
2025-10-31 10:11:43 +0530

    Login Blade Change 2025-10-31

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,69 +4,76 @@
 <main>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-lg-4">
-                <div class="card shadow-lg border-0 rounded-lg mt-5">
-                    <img src="{{url('/public/images/hrm.png')}}" class="card-img-top" alt="">
-                    <div class="card-body">
-                        <form class="form-horizontal" method="POST" action="{{ route('login') }}" autocomplete="off">
-                            {{ csrf_field() }}
-                            <div class="form-group mb-1 {{ $errors->has('email') ? ' has-error' : '' }}"><label
-                                    class="small mb-2" for="inputEmailAddress">Email</label><input
-                                    class="form-control form-control-sm" name="email" id="email" type="email"
-                                    placeholder="Enter email address" value="{{ old('email') }}" required autofocus />
-                            </div>
-                            @if ($errors->has('email'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                            @endif
-                            <div class="form-group mb-1 {{ $errors->has('password') ? ' has-error' : '' }}">
-                                <label class="small mb-2" for="inputPassword">Password</label>
-                                <div class="input-group">
-                                    <input style="height: 35px;" class="form-control form-control-sm" id="password"
-                                        name="password" type="password" placeholder="Enter password" required />
-                                    <div class="input-group-append">
-                                        <span class="input-group-text" onclick="togglePasswordVisibility()"
-                                            style="cursor: pointer;">
-                                            <i class="fas fa-eye" id="toggleIcon"></i>
-                                        </span>
+            <div class="col-lg-10 mt-sm-0 mt-lg-5 pt-sm-0 pt-lg-5">
+                <div class="card mt-5 mb-5">
+                    <div class="row no-gutters">
+                        <div class="col-md-5">
+                            <img src="{{url('/public/images/hrm.png')}}" class="card-img-top" alt="">
+                        </div>
+                        <div class="col-md-7">
+                            <div class="card-body">
+                                <form class="form-horizontal mt-sm-0 mt-lg-3" method="POST" action="{{ route('login') }}" autocomplete="off">
+                                    {{ csrf_field() }}
+                                    <div class="form-group mb-1 {{ $errors->has('email') ? ' has-error' : '' }}"><label
+                                            class="small mb-2" for="inputEmailAddress">Email</label><input
+                                            class="form-control form-control-sm" name="email" id="email" type="email"
+                                            placeholder="Enter email address" value="{{ old('email') }}" required autofocus />
+                                    </div>
+                                    @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                    @endif
+                                    <div class="form-group mb-1 {{ $errors->has('password') ? ' has-error' : '' }}">
+                                        <label class="small mb-2" for="inputPassword">Password</label>
+                                        <div class="input-group">
+                                            <input style="height: 35px;" class="form-control form-control-sm" id="password"
+                                                name="password" type="password" placeholder="Enter password" required />
+                                            <div class="input-group-append">
+                                                <span class="input-group-text" onclick="togglePasswordVisibility()"
+                                                    style="cursor: pointer;">
+                                                    <i class="fas fa-eye" id="toggleIcon"></i>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                    @endif
+                                    <div class="form-group">
+                                        <div class="custom-control custom-checkbox mt-2">
+                                            <input type="checkbox" class="custom-control-input" name="remember"
+                                                id="rememberPasswordCheck" {{ old('remember') ? 'checked' : '' }}>
+                                            <label class="custom-control-label" for="rememberPasswordCheck">Remember
+                                                password</label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group text-right mt-4 mb-0">
+                                        <button type="submit" class="btn btn-primary btn-sm px-4">Login</button>
+                                    </div>
+                                </form>
+                                <hr>
+                                <div class="row justify-content-between mt-2">
+                                    <div class="col">
+                                        <a href="https://play.google.com/store/apps/details?id=com.shapeup.hr"
+                                            class="text-decoration-none text-dark" target="_blank"><img
+                                                src="images/googleplay.png" alt="" class="img-fluid"></a>
+                                    </div>
+                                    <div class="col-2 align-self-center text-right">
+                                        <button type="button" class="btn btn-link text-decoration-none text-dark p-0" data-toggle="modal" data-target="#qrModal">
+                                            <i class="fal fa-qrcode fa-2x"></i>
+                                        </button>
                                     </div>
                                 </div>
-                            </div>
-
-                            @if ($errors->has('password'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('password') }}</strong>
-                            </span>
-                            @endif
-                            <div class="form-group">
-                                <div class="custom-control custom-checkbox mt-2">
-                                    <input type="checkbox" class="custom-control-input" name="remember"
-                                        id="rememberPasswordCheck" {{ old('remember') ? 'checked' : '' }}>
-                                    <label class="custom-control-label" for="rememberPasswordCheck">Remember
-                                        password</label>
-                                </div>
-                            </div>
-                            <div class="form-group text-right mt-4 mb-0">
-                                <button type="submit" class="btn btn-primary btn-sm px-4">Login</button>
-                            </div>
-                        </form>
-                        <div class="row justify-content-between mt-2">
-                            <div class="col">
-                                <a href="https://play.google.com/store/apps/details?id=com.shapeup.hr"
-                                    class="text-decoration-none text-dark" target="_blank"><img
-                                        src="images/googleplay.png" alt="" class="img-fluid"></a>
-                            </div>
-                            <div class="col-2 align-self-center">
-                                <button type="button" class="btn btn-link text-decoration-none text-dark p-0" data-toggle="modal" data-target="#qrModal">
-                                    <i class="fal fa-qrcode fa-2x"></i>
-                                </button>
                             </div>
                         </div>
                     </div>
                     <div class="card-footer bg-laugfs">
                         <div class="row">
-                            <div class="col-md-12 small text-center">
+                            <div class="col-md-12 small text-right">
                                 Copyright &copy; <a href="https://erav.lk/" target="_blank" class="no-link-style">eRav
                                     Technology</a> <?php echo date('Y') ?>
                             </div>
