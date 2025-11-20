46a3f2dec40f54908ef0c2faecbb9b9a445aca38
Namidu99 <nami.hasalanka@gmail.com>
2025-11-20 12:25:37 +0530

    Fix ID references in administrator navigation bar links

--- resources/views/layouts/administrator_nav_bar.blade.php
+++ resources/views/layouts/administrator_nav_bar.blade.php
@@ -3,11 +3,11 @@
 
       <div class="dropdown">
        
-        <a role="button" class="btn navbtncolor" href="{{ route('users.index') }}" id="users_link">Users <span class="caret"></span></a>
+        <a role="button" class="btn navbtncolor" href="{{ route('users.index') }}" id="user_link">Users <span class="caret"></span></a>
        
-        <a role="button" class="btn navbtncolor" href="{{ route('roles.index') }}" id="roles_link">Roles <span class="caret"></span></a>
+        <a role="button" class="btn navbtncolor" href="{{ route('roles.index') }}" id="role_link">Roles <span class="caret"></span></a>
        
-        <a role="button" class="btn navbtncolor" href="{{ route('permissions.index') }}" id="roles_link">Permissions <span class="caret"></span></a>
+        <a role="button" class="btn navbtncolor" href="{{ route('permissions.index') }}" id="permissions_link">Permissions <span class="caret"></span></a>
        
       </div>
     </div>
