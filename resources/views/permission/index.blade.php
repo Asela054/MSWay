8b952d10a8fca114dceecbedd150ec5c16793973
Namidu99 <nami.hasalanka@gmail.com>
2025-11-20 12:25:47 +0530

    Refactor permission management UI: update button labels and table headers for clarity

--- resources/views/permission/index.blade.php
+++ resources/views/permission/index.blade.php
@@ -22,10 +22,7 @@
                 <div class="card-body p-0 p-2">
                     <div class="row">
                         <div class="col-12 text-right">
-                            @can('role-create')
                                 <button type="button" class="btn btn-primary btn-sm px-4" name="create_record" id="create_record"><i class="fas fa-plus mr-2"></i>Create New Permission</button>
-
-                            @endcan
                         </div>
                         <div class="col-12">
                             <hr class="border-dark">
@@ -41,10 +38,10 @@
                              <table class="table table-striped table-sm" width="100%" id="permissiontable">
                                 <thead>
                                 <tr>
-                                    <th>No</th>
-                                    <th>Name</th>
-                                    <th>Module</th>
-                                    <th width="280px">Action</th>
+                                    <th>NO</th>
+                                    <th>PERMISSION</th>
+                                    <th>MODULE</th>
+                                    <th width="280px">ACTION</th>
                                 </tr>
                                 </thead>
                                 
@@ -79,13 +76,13 @@
                                 <!-- Permission Name -->
                                 <div class="col-xs-12 col-sm-12 col-md-12">
                                     <div class="form-group">
-                                        <strong>Name:</strong>
+                                        <strong>Permission:</strong>
                                         <input 
                                             type="text" 
                                             name="name" 
                                             id="name" 
                                             class="form-control form-control-sm" 
-                                            placeholder="Name" 
+                                            placeholder="Permission Name" 
                                             value="{{ old('name') }}">
                                         
                                     </div>
@@ -94,7 +91,7 @@
                                 <!-- Module List -->
                                 <div class="col-xs-12 col-sm-12 col-md-12">
                                     <div class="form-group">
-                                        <strong>Permission:</strong><br>
+                                        <strong>Module:</strong><br>
                                         <input 
                                             list="modules" 
                                             name="module" 
@@ -262,7 +259,7 @@
                             var is_resigned = row.is_resigned;
                             var buttons = '';
 
-                        buttons += '<button name="view" id="'+row.id+'" class="view btn btn-info btn-sm mr-1" type="button" data-toggle="tooltip" title="View"><i class="fa fa-eye"></i></button>';
+                        // buttons += '<button name="view" id="'+row.id+'" class="view btn btn-info btn-sm mr-1" type="button" data-toggle="tooltip" title="View"><i class="fa fa-eye"></i></button>';
                         buttons += '<button name="edit" id="'+row.id+'" class="edit btn btn-primary btn-sm mr-1" type="button" data-toggle="tooltip" title="Edit"><i class="fas fa-pencil-alt"></i></button>';
                         buttons += '<button type="submit" name="delete" id="'+row.id+'" class="delete btn btn-danger btn-sm  mr-1" data-toggle="tooltip" title="Remove"><i class="far fa-trash-alt"></i></button>';
 
