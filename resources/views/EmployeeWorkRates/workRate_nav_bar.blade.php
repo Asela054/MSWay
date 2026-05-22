8029eadbe886e0f0e7d48e63fae2b7c1593a31fc
Namidu99 <nami.hasalanka@gmail.com>
2026-05-22 14:06:54 +0530

    Add work rate navigation bar to Employee Work Rates view

---
+++ resources/views/EmployeeWorkRates/workRate_nav_bar.blade.php
@@ -0,0 +1,6 @@
+<div class="row nowrap" style="padding-top: 5px;padding-bottom: 5px;">
+  <div class="dropdown">
+    <a role="button" data-toggle="dropdown" class="btn navbtncolor" href="{{ route('empworkrate')}}" id="emp_work_rate">
+      Work rate <span class="caret"></span></a>
+  </div>
+</div>
