b1b81471d8a45f97306233273b33cce4286c0c8f
Namidu99 <nami.hasalanka@gmail.com>
2026-05-29 01:11:13 +0530

    add EmployeeLetter buttons

--- resources/views/layouts/employee_nav_bar.blade.php
+++ resources/views/layouts/employee_nav_bar.blade.php
@@ -20,18 +20,12 @@
   <a role="button" class="btn navbtncolor" href="{{ route('addEmployee') }}" id="employeeinformation">Employee Details <span class="caret"></span></a>
 
   <div class="dropdown">
-    <a role="button" data-toggle="dropdown" class="btn navbtncolor" href="#" id="appointmentletter">
+    <a role="button" data-toggle="dropdown" class="btn navbtncolor" href="#" id="employeeletter">
         Employee Letters <span class="caret"></span></a>
         <ul class="dropdown-menu multi-level dropdownmenucolor" role="menu" aria-labelledby="dropdownMenu">
-          <li><a class="dropdown-item" href="{{ route('appoinementletter')}}" id="">Employee Appointment Letter</a></li>
-          <li><a class="dropdown-item" href="{{ route('jobconfirmationletter')}}">Employee Job Confirmation Letter</a></li>
-          <li><a class="dropdown-item" href="{{ route('NDAletter')}}">Employee NDA Letter</a></li>
-          <li><a class="dropdown-item" href="{{ route('warningletter')}}">Employee Warning Letter</a></li>
-          <li><a class="dropdown-item" href="{{ route('salary_incletter')}}">Employee Salary Increment Letter</a></li>
-          <li><a class="dropdown-item" href="{{ route('promotionletter')}}">Employee Promotion Letter</a></li>
-          <li><a class="dropdown-item" href="{{ route('serviceletter')}}">Employee Service Letter</a></li>
-          <li><a class="dropdown-item" href="{{ route('resignletter')}}">Employee Resignation Letter</a></li>
-          <li><a class="dropdown-item" href="{{ route('end_user_letter')}}">Employee End User Letter</a></li>
+          <li><a class="dropdown-item" href="{{ route('letterType')}}" id="">Employee Letter Type</a></li>
+          <li><a class="dropdown-item" href="{{ route('letterTemplate')}}">Employee Letter Template</a></li>
+           <li><a class="dropdown-item" href="{{ route('issueLetter')}}">Employee Issued Letter</a></li>
         </ul>
   </div>
 
