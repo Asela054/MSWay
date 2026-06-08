b2d9c67739f4a95ec9f35892acf4046c7f192750
Asela054 <asela.indrajith@gmail.com>
2026-06-08 10:14:44 +0530

    Absent to nopay controller & view update

--- app/Http/Controllers/DepartmentviseNopayController.php
+++ app/Http/Controllers/DepartmentviseNopayController.php
@@ -12,6 +12,7 @@ use Validator;
 use DB;
 use Datatables;
 use Carbon\Carbon;
+use Carbon\CarbonPeriod;
 
 class DepartmentviseNopayController extends Controller
 {
@@ -33,8 +34,12 @@ class DepartmentviseNopayController extends Controller
         }
         
         $department=$request->input('department');
-        $firstDate =  $request->input('from_date');
+        // $firstDate =  $request->input('from_date');
+        $fromDate =  $request->input('from_date');
+        $toDate = $request->input('to_date');
 
+        $period = CarbonPeriod::create($fromDate, $toDate)->toArray();
+        
         // Get accessible employee IDs based on user access rights
             $userId = Auth::id();
             $accessibleEmployeeIds = UserHelper::getAccessibleEmployeeIds($userId);
@@ -52,7 +57,7 @@ class DepartmentviseNopayController extends Controller
 
 
         $datareturn = [];
-
+        
         $query =  DB::table('employees')
             ->select('emp_name_with_initial as emp_name','id as emp_autoid','emp_department','emp_id as empid','emp_location')
             ->where('emp_department', '=', $department)
@@ -63,35 +68,40 @@ class DepartmentviseNopayController extends Controller
                 return $q->whereIn('emp_location', $userBranchIds);
             })
             ->get();
-
+            
             foreach ($query as $row) {
                 $empId = $row->empid;
                 $empName = $row->emp_name;
                 $empautoid = $row->emp_autoid;
+                
+                foreach ($period as $date) {
+                    $firstDate = $date->format('Y-m-d');
 
-                $attendance = DB::table('attendances')
-                ->select('attendances.*')
-                ->where('uid', $empId)
-                ->where('date', $firstDate)
-                ->whereNull('deleted_at')
-                ->first();
-
-                if(!$attendance){
-
-                    $leave = DB::table('leaves')
-                    ->select('leaves.*')
-                    ->where('emp_id', $empId)
-                    ->where('leave_from', '<=', $firstDate)
-                    ->where('leave_to', '>=', $firstDate)
-                    ->where('status', 'Approved')
+                    $attendance = DB::table('attendances')
+                    ->select('attendances.*')
+                    ->where('uid', $empId)
+                    ->where('date', $firstDate)
+                    ->whereNull('deleted_at')
                     ->first();
 
-                    if(!$leave){
-                        $datareturn[] = [
-                            'empid' => $empId,
-                            'emp_name' => $empName,
-                            'emp_autoid' => $empautoid,
-                        ];     
+                    if(!$attendance){
+
+                        $leave = DB::table('leaves')
+                        ->select('leaves.*')
+                        ->where('emp_id', $empId)
+                        ->where('leave_from', '<=', $firstDate)
+                        ->where('leave_to', '>=', $firstDate)
+                        ->where('status', 'Approved')
+                        ->first();
+
+                        if(!$leave){
+                            $datareturn[] = [
+                                'empid' => $empId,
+                                'emp_name' => $empName,
+                                'emp_autoid' => $empautoid,
+                                'emp_date' => $firstDate,
+                            ];     
+                        }
                     }
                 }
               
@@ -115,21 +125,35 @@ class DepartmentviseNopayController extends Controller
 
         $current_date_time = Carbon::now()->toDateTimeString();
 
-        $isSaturday = Carbon::parse($leavedate)->isSaturday();
-
-        $no_of_days = $isSaturday ? '0.5' : '1'; 
-
         foreach ($dataarry as $row) {
 
             $empid = $row['empid'];
             $epfno = $row['emp_name'];
             $emp_autoid = $row['emp_autoid'];
+            $emp_date = $row['emp_date'];
 
+            $jobcategory = DB::table('employees')
+                ->leftjoin('job_categories', 'employees.job_category_id', '=', 'job_categories.id')
+                ->where('emp_id', $empid)
+                ->select('job_categories.is_sat_ot_type_as_act')
+                ->first();
+
+            $isSaturday = Carbon::parse($emp_date)->isSaturday();
+
+            if ($isSaturday && $jobcategory->is_sat_ot_type_as_act == 2) {
+                $saturdaynodays = '1';
+            } else if ($isSaturday && $jobcategory->is_sat_ot_type_as_act == 1) { 
+                $saturdaynodays = '0.5';
+            }else {
+                $saturdaynodays = '1';
+            }
+
+            $no_of_days = $isSaturday ? $saturdaynodays : '1'; 
 
             $leave = Leave::where('emp_id', $empid)
               ->where('leave_type', $leavetype)
-              ->whereDate('leave_from', '<=', $leavedate)
-              ->whereDate('leave_to', '>=', $leavedate)
+              ->whereDate('leave_from', '<=', $emp_date)
+              ->whereDate('leave_to', '>=', $emp_date)
               ->first();
               if ($leave) {
                 $leave->update([
@@ -151,8 +175,8 @@ class DepartmentviseNopayController extends Controller
             $leave = new Leave;
             $leave->emp_id = $empid;
             $leave->leave_type = $leavetype;
-            $leave->leave_from = $leavedate;
-            $leave->leave_to = $leavedate;
+            $leave->leave_from = $emp_date;
+            $leave->leave_to = $emp_date;
             $leave->no_of_days = $no_of_days;
             $leave->half_short = $no_of_days;
             $leave->reson = 'No Covering';
@@ -169,6 +193,6 @@ class DepartmentviseNopayController extends Controller
             $leave->save();
             }
         }
-        return response()->json(['success' => 'Absent Nopay is successfully Approved']);
+        return response()->json(['success' => 'Absent Leave is successfully Approved']);
     }
 }
