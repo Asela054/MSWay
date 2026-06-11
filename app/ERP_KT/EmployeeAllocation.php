ab9b5e6e3109d7b92e9535cc743838631474783d
Chamith Dilshan Tharuka <dilshanchamith99@gmail.com>
2026-06-11 17:06:31 +0530

    Add Employee Allocation model and list script for managing shift allocations

---
+++ app/ERP_KT/EmployeeAllocation.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\ERP_KT;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EmployeeAllocation extends Model
+{
+    protected $table = 'kt_shift_ot';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+
+    protected $fillable = [
+        'shift_id',
+        'emp_id',
+        'date',
+        'in_time',
+        'out_time',
+    ];
+}
